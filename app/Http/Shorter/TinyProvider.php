3771de257911b81b751c91d3c6f0ee956f3c239a
volkan catak <iletisim@volkancatak.com.tr>
2022-10-23 20:23:50 +0300

    Tiny Provider added.

---
+++ app/Http/Shorter/TinyProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+    namespace App\Http\Shorter;
+
+    use Illuminate\Support\Facades\Http;
+
+    class TinyProvider implements ShortenerInterface
+    {
+        /**
+         * Generate short url from tinyurl.com
+         *
+         * @param string $url
+         * @return object
+         */
+        public function shortUrl(string $url): object
+        {
+            $responseData = Http::withHeaders([
+                'Authorization' => 'Bearer ' . config('services.tiny.token'),
+                'Content-Type'  => 'application/json',
+                'Accept'        => 'application/json',
+            ])->post('https://api.tinyurl.com/create', [
+                "url"    => $url,
+                "domain" => config('services.tiny.domain'),
+            ]);
+
+            // Return PHP object for further processing
+            return $responseData->object();
+
+        }
+    }
