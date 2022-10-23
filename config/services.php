a392a18b7541cc3d4b36870386514b8ce33bfea9
volkan catak <iletisim@volkancatak.com.tr>
2022-10-23 19:42:33 +0300

    Tiny.url configuration field added.

--- config/services.php
+++ config/services.php
@@ -31,4 +31,9 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'tiny' => [
+        'token' => env('SHORT_URL_TINY_TOKEN'),
+        'domain' => env('SHORT_URL_TINY_DOMAIN'),
+    ],
+
 ];
