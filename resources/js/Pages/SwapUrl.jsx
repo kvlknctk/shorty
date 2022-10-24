6ec2cdbde559bec2cce4c562052bf5e2893f12be
volkan catak <iletisim@volkancatak.com.tr>
2022-10-24 20:17:26 +0300

    Added small component, dummy version data, provider selector to main component.

--- resources/js/Pages/SwapUrl.jsx
+++ resources/js/Pages/SwapUrl.jsx
@@ -1,20 +1,40 @@
 import React, {useState} from 'react';
 import {Head} from '@inertiajs/inertia-react';
 import axiosEngine from "@/Engines/axiosEngine";
+import ShortedUrlItem from "@/Components/ShortedUrlItem";
 
 export default function SwapUrl(props) {
 
-    const [url, setUrl] = useState('');
     const [urls, setUrls] = useState([]);
 
+    const [formData, setFormData] = useState({
+        url: '',
+        provider: 'tiny'
+    });
+
     const handlePostIt = () => {
-        axiosEngine.post('/create', {url})
+        axiosEngine.post('/short', formData)
             .then(res => res.data)
             .then((response) => {
-                setUrls([...urls, response.data.shortedUrl]);
-            });
+                setUrls([...urls, response]);
+            }).catch(err => {
+            alert(err.response.data.message)
+        });
     }
 
+    function handleProviderChange(e) {
+        setFormData({
+            ...formData,
+            provider: e.target.value
+        });
+    }
+
+    function handleUrl(e) {
+        setFormData({
+            ...formData,
+            url: e.target.value
+        });
+    }
 
     return (
         <>
@@ -27,21 +47,30 @@ export default function SwapUrl(props) {
                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                             Paste Long URL
                         </label>
-                        <input autoFocus value={url}
-                               onChange={e => setUrl(e.target.value)}
-                               type="text" id="longUrl"
-                               className="urlInput"
-                               placeholder="https://"
-                        />
-                        <button type="button" disabled={!url} onClick={() => handlePostIt()}
+                        <div className="flex flex-row">
+
+                            <input autoFocus value={formData.url}
+                                   onChange={e => handleUrl(e)}
+                                   type="text" id="longUrl"
+                                   className="urlInput flex-[2]"
+                                   placeholder="https://"
+                            />
+
+                            <select id="provider" value={formData.provider} className="shorterProviderSelector flex-[1]"
+                                    onChange={e => handleProviderChange(e)}>
+                                <option value="tiny">TinyURL</option>
+                                <option value="bitly">Bitly</option>
+                            </select>
+
+                        </div>
+                        <button type="button" disabled={!formData.url} onClick={() => handlePostIt()}
                                 className="createButton">Post it
                         </button>
+                        <div className="text-xs text-gray-600 text-center ">
+                            {props.dummyAppVersion}
+                        </div>
 
-                        {
-                            urls.map((url, index) => (
-                                <div key={index} className="text-white select-all p-2 border rounded my-2 bg-gray-600">{url}</div>
-                            ))
-                        }
+                        {urls.map((url, index) => <ShortedUrlItem key={index} url={url.shortedUrl}/>)}
 
                     </div>
                 </div>
